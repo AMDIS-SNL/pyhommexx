c3a6bf773a8a72a11ed6ad7ba607fee11564eef3
Peter Bosler <pabosle@sandia.gov>
2026-08-17 20:09:30 -0600

    updating cluster env

--- env/cee-amdis-modules.sh
+++ env/cee-amdis-modules.sh
@@ -4,8 +4,10 @@ module load aue/cuda/12.4.0-gcc-12.3.0
 module load aue/openmpi/4.1.6-gcc-12.3.0-cuda-12.4.0
 module load aue/hdf5/1.14.3-gcc-12.3.0-openmpi-4.1.6
 module load aue/netcdf-c/4.9.2-gcc-12.3.0-openmpi-4.1.6
+module load aue/netlib-lapack/3.11.0-gcc-12.3.0
 module load aue/cmake/3.31.6
 module load aue/binutils/2.45
 module load aue/python/3.13.2
 export LD_LIBRARY_PATH="$NETCDF_ROOT/lib:$HDF5_ROOT/lib:$LD_LIBRARY_PATH"
+export SCRATCH="/gpfs/$USER"
 
