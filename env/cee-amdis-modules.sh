85ebc957273ef075d3a676d03a1ea1c28239c57b
Peter Bosler <pabosle.sandia.gov>
2026-08-20 10:46:39 -0600

    adding mpi to ld_library_path on cee

--- env/cee-amdis-modules.sh
+++ env/cee-amdis-modules.sh
@@ -1,13 +1,13 @@
 module purge
 module load aue/gcc/12.3.0
 module load aue/cuda/12.4.0-gcc-12.3.0
-module load aue/openmpi/4.1.6-gcc-12.3.0-cuda-12.4.0
 module load aue/hdf5/1.14.3-gcc-12.3.0-openmpi-4.1.6
 module load aue/netcdf-c/4.9.2-gcc-12.3.0-openmpi-4.1.6
 module load aue/netlib-lapack/3.11.0-gcc-12.3.0
 module load aue/cmake/3.31.6
 module load aue/binutils/2.45
 module load aue/python/3.13.2
-export LD_LIBRARY_PATH="$NETCDF_ROOT/lib:$HDF5_ROOT/lib:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$NETCDF_ROOT/lib:$HDF5_ROOT/lib:/projects/aue/cee/deploy/9d138342/linux-rhel8-x86_64/gcc-12.3.0/openmpi-4.1.6-hfp5vwq/lib:$LD_LIBRARY_PATH"
 export SCRATCH="/gpfs/$USER"
+export PYHOMMEXX_LIB_PATH="${SCRATCH}/build-e3sm-amdis/src/theta-l_kokkos/pyhommexx/"
 
